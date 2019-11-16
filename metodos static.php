584417420f7384bfd90fdb540519da22faf3011d
Cássio Klebio Pereira Lins <cassiotr2009@gmail.com>
2019-11-15 21:09:06 -0300

    Add files via upload
    Estudo sobre metodos estaticos.

---
+++ metodos static.php
@@ -0,0 +1,26 @@
+<?php
+
+class Login {
+    
+    public static $user;
+    
+    public static function verificaLogin()
+    {
+        echo "O usuario ".self::$user. "  está logado";
+    }
+    
+    public function sairSistema(){
+        echo "O usuário deslogou";
+    }
+}
+
+
+Login::$user = "admin";
+Login::verificaLogin();
+echo "<br>";
+
+$login = new Login();
+$login->sairSistema();
+
+
+?>
